994439747fe9b869b6e00dbe0b167614cbae4656
nth233 <fpg2012@foxmail.com>
2025-12-01 19:29:53 +0800

    support MacOS (Apple Silicon)

--- _converters/convert.sh
+++ _converters/convert.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+os_type=$(uname -s)
 
-pandoc -t html --filter pandoc-katex --filter pandocfilter-pygments.py
+case "$os_type" in
+	Linux*)	pandoc -t html --filter pandoc-katex --filter pandocfilter-pygments.py;;
+	Darwin*) pandoc -t html --filter pandoc-katex-darwin --filter pandocfilter-pygments.py;;
+	*)	echo "Unknown OS: $os_type"
+esac
